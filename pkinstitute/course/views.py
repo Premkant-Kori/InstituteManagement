edbeb4786edee57d97aef7818707762f52c17ce1
Premkant Kori <101425848+Premkant-Kori@users.noreply.github.com>
2023-07-24 18:40:00 +0530

    course ui

--- pkinstitute/course/views.py
+++ pkinstitute/course/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'all_courses.html')
+    return render(request, 'course/all_courses.html')
