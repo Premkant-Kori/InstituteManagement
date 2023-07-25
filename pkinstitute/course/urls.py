f5c9ff3705fc02fd22971f3af00272316aa98c13
Premkant Kori <101425848+Premkant-Kori@users.noreply.github.com>
2023-07-25 15:31:29 +0530

    active page highlight

--- pkinstitute/course/urls.py
+++ pkinstitute/course/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('course/', views.course, name='course'),
 
 ]
--- pkinstitute/course/views.py
+++ pkinstitute/course/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+active = {
+    'nav': 'active'
+}
+
+
 def home(request):
-    return render(request, 'course/all_courses.html')
+    return render(request, 'base.html', {'nav': 'home'})
+
+
+def course(request):
+    return render(request, 'course/all_courses.html', {'nav': 'course'})
