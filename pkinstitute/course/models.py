76d228e026bfd63e94b9bcc8416588ea1045ecb8
Premkant Kori <101425848+Premkant-Kori@users.noreply.github.com>
2023-07-25 17:52:29 +0530

    created models 1.1

--- pkinstitute/course/models.py
+++ pkinstitute/course/models.py
@@ -5,6 +5,5 @@ from django.db import models
 class Course(models.Model):
     course_title = models.CharField(max_length=30, null=False, blank=False)
     course_desc = models.TextField(max_length=150, null=False, blank=False)
-
     def __str__(self):
         return self.course_title
